2be98a2585cf3cc01de80d470e91f08dcad2ac56
azu <azuciao@gmail.com>
2021-04-08 17:50:06 +0900

    fix(deps): upgrade sentence-splitter@3

--- src/max-ten.js
+++ src/max-ten.js
@@ -1,56 +1,42 @@
 // LICENSE : MIT
 "use strict";
-import {RuleHelper} from "textlint-rule-helper"
-import {getTokenizer} from "kuromojin";
-import {split as splitSentences} from "sentence-splitter";
-import Source from "structured-source";
+import { RuleHelper } from "textlint-rule-helper"
+import { getTokenizer } from "kuromojin";
+import { splitAST, Syntax as SentenceSyntax } from "sentence-splitter";
+import { StringSource } from "textlint-util-to-string";
+
 const defaultOptions = {
     max: 3, // 1文に利用できる最大の、の数
     strict: false // 例外ルールを適応するかどうか
 };
 
 function isSandwichedMeishi({
-    before,
-    token,
-    after
-}) {
+                                before,
+                                token,
+                                after
+                            }) {
     if (before === undefined || after === undefined || token === undefined) {
         return false;
     }
     return before.pos === "名詞" && after.pos === "名詞";
 }
-/**
- * add two positions.
- * note: line starts with 1, column starts with 0.
- * @param {Position} base
- * @param {Position} relative
- * @return {Position}
- */
-function addPositions(base, relative) {
-    return {
-        line: base.line + relative.line - 1, // line 1 + line 1 should be line 1
-        column: relative.line == 1 ? base.column + relative.column // when the same line
-            : relative.column               // when another line
-    };
-}
+
 /**
  * @param {RuleContext} context
  * @param {object} [options]
  */
-module.exports = function(context, options = {}) {
+module.exports = function (context, options = {}) {
     const maxLen = options.max || defaultOptions.max;
     const isStrict = options.strict || defaultOptions.strict;
-    let helper = new RuleHelper(context);
-    let {Syntax, RuleError, report, getSource} = context;
+    const helper = new RuleHelper(context);
+    const { Syntax, RuleError, report, getSource } = context;
     return {
-        [Syntax.Paragraph](node){
+        [Syntax.Paragraph](node) {
             if (helper.isChildNode(node, [Syntax.BlockQuote])) {
                 return;
             }
-            let sentences = splitSentences(getSource(node), {
-                charRegExp: /[。\?\!？！]/,
-                newLineCharacters: "\n\n"
-            });
+            const resultNode = splitAST(node);
+            const sentences = resultNode.children.filter(childNode => childNode.type === SentenceSyntax.Sentence);
             /*
              <p>
              <str><code><img><str>
@@ -65,10 +51,10 @@ module.exports = function(context, options = {}) {
              */
             return getTokenizer().then(tokenizer => {
                 sentences.forEach(sentence => {
-                    let text = sentence.value;
-                    let source = new Source(text);
+                    const source = new StringSource(sentence);
+                    const text = source.toString();
+                    const tokens = tokenizer.tokenizeForSentence(text);
                     let currentTenCount = 0;
-                    let tokens = tokenizer.tokenizeForSentence(text);
                     let lastToken = null;
                     tokens.forEach((token, index) => {
                         let surface = token.surface_form;
@@ -92,11 +78,10 @@ module.exports = function(context, options = {}) {
                         }
                         // report
                         if (currentTenCount >= maxLen) {
-                            let positionInSentence = source.indexToPosition(lastToken.word_position - 1);
-                            let positionInNode = addPositions(sentence.loc.start, positionInSentence);
-                            let ruleError = new context.RuleError(`一つの文で"、"を${maxLen}つ以上使用しています`, {
-                                line: positionInNode.line - 1,
-                                column: positionInNode.column
+                            const positionInSentence = source.originalIndexFromIndex(lastToken.word_position - 1);
+                            const index = sentence.range[0] + positionInSentence;
+                            const ruleError = new context.RuleError(`一つの文で"、"を${maxLen}つ以上使用しています`, {
+                                index
                             });
                             report(node, ruleError);
                             currentTenCount = 0;
